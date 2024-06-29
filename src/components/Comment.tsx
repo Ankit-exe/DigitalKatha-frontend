78e7bb0b3bf2e189c71fabd0107ca5000e5534ea
Ankit.exe <ankitghimire321@gmail.com>
2024-06-29 19:55:12 +0545

    edit comment

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,3 +1,4 @@
+import { Button, Textarea } from "flowbite-react";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import { FaThumbsUp } from "react-icons/fa";
@@ -11,9 +12,11 @@ interface UserData {
 
 const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const Comment = ({ comment, onLike }: any) => {
+export const Comment = ({ comment, onLike, onEdit }: any) => {
   const [user, setUser] = useState<UserData | null>(null);
   const { currentUser } = useSelector((state: any) => state.user);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(comment.content);
 
   useEffect(() => {
     const getUser = async () => {
@@ -33,6 +36,35 @@ export const Comment = ({ comment, onLike }: any) => {
     getUser();
   }, [comment.userId]);
 
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedContent(comment.content);
+  };
+
+  const handlesave = async () => {
+    try {
+      const res = await fetch(
+        `${API_BASE_URL}/api/comment/editComment/${comment._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            content: editedContent,
+          }),
+        }
+      );
+      if (res.ok) {
+        setIsEditing(false);
+        onEdit(comment, editedContent);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       {user === null ? (
@@ -46,7 +78,7 @@ export const Comment = ({ comment, onLike }: any) => {
               alt={user.username}
             />
           </div>
-          <div>
+          <div className="w-full">
             <div className="flex flex-1 flex-row items-center">
               <span className="font-bold mr-1 text-xm truncate">
                 {user ? `@${user.username}` : "anonymous user"}
@@ -55,26 +87,66 @@ export const Comment = ({ comment, onLike }: any) => {
                 {moment(comment.createdAt).fromNow()}
               </span>
             </div>
-            <p className=" text-gray-500 mb-2">{comment.content}</p>
-            <div className="flex flex-row gap-1 items-center pt-2 text-sm border-t border-gray-200 dark:border-gray-700 max-w-fit">
-              <button
-                type="button"
-                onClick={() => onLike(comment._id)}
-                className={`text-gray-400 hover:text-pink-500 ${
-                  currentUser &&
-                  comment.likes.includes(currentUser.userId) &&
-                  "!text-pink-500"
-                }`}
-              >
-                <FaThumbsUp className="text-sm " />
-              </button>
-              <p className="text-gray-400">
-                {comment.numberOfLikes > 0 &&
-                  comment.numberOfLikes +
-                    " " +
-                    (comment.numberOfLikes === 1 ? "like" : "likes")}
-              </p>
-            </div>
+            {isEditing ? (
+              <>
+                <Textarea
+                  className="mb-2"
+                  value={editedContent}
+                  onChange={(e) => setEditedContent(e.target.value)}
+                />
+                <div className="flex flex-row gap-3 justify-end text-sm">
+                  <Button
+                    type="button"
+                    size="sm"
+                    gradientDuoTone="purpleToPink"
+                    onClick={handlesave}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    size="sm"
+                    type="button"
+                    outline
+                    gradientDuoTone="purpleToPink"
+                    onClick={() => setIsEditing(false)}
+                  >
+                    Cancle
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <>
+                <p className=" text-gray-500 mb-2">{comment.content}</p>
+                <div className="flex flex-row gap-1 items-center pt-2 text-sm border-t border-gray-200 dark:border-gray-700 max-w-fit">
+                  <button
+                    type="button"
+                    onClick={() => onLike(comment._id)}
+                    className={`text-gray-400 hover:text-pink-500 ${
+                      currentUser &&
+                      comment.likes.includes(currentUser.userId) &&
+                      "!text-pink-500"
+                    }`}
+                  >
+                    <FaThumbsUp className="text-sm " />
+                  </button>
+                  <p className="text-gray-400">
+                    {comment.numberOfLikes > 0 &&
+                      comment.numberOfLikes +
+                        " " +
+                        (comment.numberOfLikes === 1 ? "like" : "likes")}
+                  </p>
+                  {currentUser && currentUser.userId === comment.userId && (
+                    <button
+                      type="button"
+                      onClick={handleEdit}
+                      className="text-gray-400 hover:text-pink-500 "
+                    >
+                      Edit
+                    </button>
+                  )}
+                </div>
+              </>
+            )}
           </div>
         </div>
       )}
--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -104,6 +104,14 @@ export const CommentSection = ({ postId }: Props) => {
     }
   };
 
+  const handleEdit = async (comment: any, editedContent: any) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
+
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -168,7 +176,12 @@ export const CommentSection = ({ postId }: Props) => {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment
+              key={comment._id}
+              comment={comment}
+              onLike={handleLike}
+              onEdit={handleEdit}
+            />
           ))}
         </>
       )}
