f3910afb104087261ab838e9174e61643d6eb47e
Ankit.exe <ankitghimire321@gmail.com>
2024-07-04 21:39:26 +0545

    deployment ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { SignIn } from "./pages/SignIn";
@@ -120,6 +120,7 @@ function App() {
           }
         />
       </Routes>
+      <Route path="*" element={<Navigate to="/" />} />
     </BrowserRouter>
   );
 }
